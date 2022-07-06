d08f01b57df04342ae1a796e7ebdcd4400831721
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-07-06 08:50:02 +0700

    first commit

---
+++ src/main/java/com/example/nhom4_quanlytaichinh/model/Account.java
@@ -0,0 +1,9 @@
+package com.example.nhom4_quanlytaichinh.model;
+
+public class Account {
+    private Long soTaiKhoan;
+    private String tenTaiKhoan;
+    private double soTienTrongTaiKhoan;
+    
+
+}
